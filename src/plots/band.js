4799ea78dd016a2ca9c9dc79f0c535b955428cee
Dakota Gordon <dakota@via.world>
2018-11-09 18:37:28 -0600

    Color bands with options

--- src/plots/band.js
+++ src/plots/band.js
@@ -14,16 +14,17 @@ module.exports = {
             default: true
         }
     },
-    render: ({chart, panel, element, data, parameters}) => {
+    render: ({chart, panel, element, data, parameters, options}) => {
         const bands = element.selectAll('rect').data(data);
 
         bands.enter()
             .append('rect')
             .merge(bands)
-                .attr('x', ([x]) => chart.scale(x))
+                .attr('x', ([x]) => Math.round(chart.scale(x) - (chart.bandwidth / 2)))
                 .attr('y', 0)
-                .attr('width', chart.bandwidth)
-                .attr('height', panel.height);
+                .attr('width', Math.ceil(chart.bandwidth))
+                .attr('height', panel.height)
+                .attr('fill', options.fill ? (d, i) => options.fill(d, i) : parameters.fill);
 
         bands.exit().remove();
     }
